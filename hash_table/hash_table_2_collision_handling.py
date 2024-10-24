416c1b9e64e655bc88c3120208d050dfa181a370
bifinbabu <bifinbabu088@gmail.com>
2024-10-24 23:04:59 +0530

    Hash table with collision handling

---
+++ hash_table/hash_table_2_collision_handling.py
@@ -0,0 +1,58 @@
+class HashTable:
+    def __init__(self):
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+
+        return h % self.MAX
+
+    # Overriding
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+
+        found = False
+        for idx, element in enumerate(self.arr[h]):
+            if len(element) == 2 and element[0] == key:
+                self.arr[h][idx] = (key, val)
+                found = True
+                break
+
+        if not found:
+            self.arr[h].append((key, val))
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        for idx, element in enumerate(self.arr[h]):
+            if element[0] == key:
+                del self.arr[h][idx]
+
+
+t = HashTable()
+print(t.get_hash("march 6"))
+print(t.get_hash("march 17"))
+
+t["march 6"] = 120
+t["march 6"] = 78
+t["march 8"] = 67
+t["march 9"] = 4
+t["march 17"] = 459
+
+print(t["march 6"])
+print(t["march 17"])
+print(t.arr)
+
+del t["march 17"]
+print(t.arr)
+
+del t["march 6"]
+print(t.arr)
